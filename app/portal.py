5e3d4de7797ef58cd7847fdcbe1aa4b74b135d5d
miltonrubens123-cmd <milton.rubens123@gmail.com>
2026-04-22 00:00:59 -0300

    fix: ajuste estrutural do portal business vision

--- app/portal.py
+++ app/portal.py
@@ -1257,8 +1257,8 @@ st.sidebar.markdown(
             {iniciais}
 
                
-        <div>
-        <div>
+        </div>
+        </div>
             <div style="font-size:12px;color:#8FA5BC;">
                 Usuário atual
             </div>
